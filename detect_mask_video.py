1e6a47d4dd11a5f4c9dee6f2c18f6b7961a18a5e
gokelnath <85857841+gokelnath@users.noreply.github.com>
2021-06-14 09:29:43 +0530

    Add files via upload
    covid 19

---
+++ detect_mask_video.py
@@ -0,0 +1,189 @@
+# USAGE
+# python detect_mask_video.py
+
+# import the necessary packages
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+from subprocess import call
+import time
+import os
+import glob
+import smtplib
+import base64
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import sys
+
+
+
+gmail_user = "iotdemopan@gmail.com"
+gmail_pwd = "paniotdemo987@"
+FROM = 'iotdemopan@gmail.com'
+TO = ['dinahar27599@gmail.com']
+
+
+def mail():
+        msg = MIMEMultipart()
+        time.sleep(1)
+        msg['Subject'] ="Face mask"
+
+        #BODY with 2 argument
+
+        #body=sys.argv[1]+sys.argv[2]
+        body="No face mask"          
+        msg.attach(MIMEText(body,'plain'))
+        time.sleep(1)
+
+
+        ###IMAGE
+        fp = open("1.jpg", 'rb')   		
+        time.sleep(1)
+        img = MIMEImage(fp.read())
+        time.sleep(1)
+        fp.close()
+        time.sleep(1)
+        msg.attach(img)
+        time.sleep(1)
+
+
+        try:
+                server = smtplib.SMTP("smtp.gmail.com", 587) #or port 465 doesn't seem to work!
+                print ("smtp.gmail")
+                server.ehlo()
+                print ("ehlo")
+                server.starttls()
+                print ("starttls")
+                server.login(gmail_user, gmail_pwd)
+                print ("reading mail & password")
+                server.sendmail(FROM, TO, msg.as_string())
+                print ("from")
+                server.close()
+                print ('successfully sent the mail')
+        except:
+                print ("failed to send mail")
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
+	# grab the dimensions of the frame and then construct a blob
+	# from it
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+		(104.0, 177.0, 123.0))
+
+	# pass the blob through the network and obtain the face detections
+	faceNet.setInput(blob)
+	detections = faceNet.forward()
+
+	# initialize our list of faces, their corresponding locations,
+	# and the list of predictions from our face mask network
+	faces = []
+	locs = []
+	preds = []
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with
+		# the detection
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the confidence is
+		# greater than the minimum confidence
+		if confidence > args["confidence"]:
+			# compute the (x, y)-coordinates of the bounding box for
+			# the object
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# ensure the bounding boxes fall within the dimensions of
+			# the frame
+			(startX, startY) = (max(0, startX), max(0, startY))
+			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+			# extract the face ROI, convert it from BGR to RGB channel
+			# ordering, resize it to 224x224, and preprocess it
+			face = frame[startY:endY, startX:endX]
+			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+			face = cv2.resize(face, (224, 224))
+			face = img_to_array(face)
+			face = preprocess_input(face)
+			face = np.expand_dims(face, axis=0)
+
+			# add the face and bounding boxes to their respective
+			# lists
+			faces.append(face)
+			locs.append((startX, startY, endX, endY))
+
+	# only make a predictions if at least one face was detected
+	if len(faces) > 0:
+		# for faster inference we'll make batch predictions on *all*
+		# faces at the same time rather than one-by-one predictions
+		# in the above `for` loop
+		preds = maskNet.predict(faces)
+
+	# return a 2-tuple of the face locations and their corresponding
+	# locations
+	return (locs, preds)
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--face", type=str,
+	default="face_detector",
+	help="path to face detector model directory")
+ap.add_argument("-m", "--model", type=str,
+	default="mask_detector.model",
+	help="path to trained face mask detector model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# load our serialized face detector model from disk
+print("[INFO] loading face detector model...")
+prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
+weightsPath = os.path.sep.join([args["face"],
+	"res10_300x300_ssd_iter_140000.caffemodel"])
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# load the face mask detector model from disk
+print("[INFO] loading face mask detector model...")
+maskNet = load_model(args["model"])
+
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+# time.sleep(2.0)
+
+# loop over the frames from the video stream
+while True:
+    frame = vs.read()
+    frame = cv2.resize(frame,(960,720))
+    color = (0, 255, 255)
+    cv2.putText(frame, "Covid-19 Face Mask Detection", (100, 50),cv2.FONT_HERSHEY_SIMPLEX, 1.5, color, 2)
+    (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+    for (box, pred) in zip(locs, preds):
+        (startX, startY, endX, endY) = box
+        (mask, withoutMask) = pred
+        label = "Face Masked" if mask > withoutMask else "No Face Mask"
+        color = (0, 255, 0) if label == "Face Masked" else (0, 0, 255)
+        if label=="No Face Mask":
+                 cv2.imwrite('1.jpg', frame)
+                 mail()
+        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+        cv2.putText(frame, label, (startX, startY - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
